16bba74070da9b4493eea75aa7cb44b8293d7ca2
Marija Havaic <marija.havaic@gmail.com>
2023-05-26 09:46:19 +0200

    Exo compteur

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [number, setNumber] = useState(0);
+    const subtract = () => {
+        setNumber(number - 1);
+    };
+    const reset = () => {
+        setNumber(0);
+    };
+    const add = () => {
+        setNumber(number + 1);
+    };
+    return (
+        <div className="App">
+            <h1>{number}</h1>
+            <button onClick={() => subtract()}>-</button>
+            <button onClick={() => reset()}>Reset</button>
+            <button onClick={() => add()}>+</button>
+        </div>
+    );
 }
 
 export default App;
